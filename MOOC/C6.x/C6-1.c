141f6ccf81fe7c9b32caecec54f95baef6718883
hellobbn <clfbbn@gmail.com>
2016-07-08 16:35:08 +0800

    MOOC/C6.x/C6-1.c: Modified through the class. 	This also have a feature of showing all numbers that are larger the average.
    	TODO:
    		1.Add choices to choose >, <, >=, <= and maybe more.
    		2.Try to optimism the source.

--- MOOC/C6.x/C6-1.c
+++ MOOC/C6.x/C6-1.c
@@ -6,7 +6,7 @@
 //  Copyright © 2016年 BBN. All rights reserved.
 //
 //  Calculate average
-//  Version: 1.0
+//  Version: 1.1
 //  Update time: 7/8/2016
 
 #include <stdio.h>
@@ -15,15 +15,26 @@ int main()
 {
     int x;
     double sum;
+    double average;
     int cnt = 0;
+    int number[100];
     scanf("%d", &x);
+    
     while (x != -1) {
+        number[cnt] = x;
         sum += x;
         cnt ++;
         scanf("%d", &x);
     }
+    average = sum/cnt;
     if (cnt > 0) {
-        printf("%f\n", sum/cnt);
+        printf("The average is:%f\n", average);
+    }
+    printf("And the numbers larger than(>=) the average(%f) are:\n", average);
+    for (int i = 0; i <=cnt; i++) {
+        if (number[i] >= average) {
+            printf(" %d ",number[i]);
+        }
     }
     return 0;
 }
