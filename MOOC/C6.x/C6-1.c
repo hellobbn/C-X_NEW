b30afbfd0cf346bf74270d71e90bca534bc29500
hellobbn <clfbbn@gmail.com>
2016-07-08 22:08:45 +0800

    MOOC/C6.x/C6-1.c: Done with TODO list 	Completed: 		1. Add status: Alpha -- support will be followed maybe for a short time 		2. Done with 4 method 		3. make the source a bit clean 	TODO: 		1.Find a function to replace Repeated stuff which can be seen in the "case()" part 	NOTE: 		May not go further after completing the TODO

--- MOOC/C6.x/C6-1.c
+++ MOOC/C6.x/C6-1.c
@@ -8,6 +8,7 @@
 //  Calculate average
 //  Version: 1.1
 //  Update time: 7/8/2016
+//  Status: Alpha.
 
 #include <stdio.h>
 
@@ -18,23 +19,77 @@ int main()
     double average;
     int cnt = 0;
     int number[100];
+    int choice = 0;
+    
     scanf("%d", &x);
     
-    while (x != -1) {
+    while (x != -1)
+    {
         number[cnt] = x;
         sum += x;
         cnt ++;
         scanf("%d", &x);
     }
     average = sum/cnt;
-    if (cnt > 0) {
+    
+    if (cnt > 0)
+    {
         printf("The average is:%f\n", average);
     }
-    printf("And the numbers larger than(>=) the average(%f) are:\n", average);
-    for (int i = 0; i <=cnt; i++) {
-        if (number[i] >= average) {
-            printf(" %d ",number[i]);
-        }
+    
+    printf("Now you can choose to show the number which have a diff to the average.");
+    printf("You can choose from the list:\n");
+    printf("1. >\n");
+    printf("2. <\n");
+    printf("3. >=\n");
+    printf("4. <=\n");
+    printf("Now Enter your choice(The number please.):");
+    scanf("%d",&choice);
+    
+    switch (choice) {
+        case 1:
+            printf("The numbers larger than(>) the average(%f) are:\n", average);
+            for (int i = 0; i <=cnt; i++)
+            {
+                if (number[i] > average)
+                {
+                    printf(" %d ",number[i]);
+                }
+            }
+            break;
+        case 2:
+            printf("The numbers larger than(<) the average(%f) are:\n", average);
+            for (int i = 0; i <=cnt; i++)
+            {
+                if (number[i] < average)
+                {
+                    printf(" %d ",number[i]);
+                }
+            }
+            break;
+        case 3:
+            printf("The numbers larger than(>=) the average(%f) are:\n", average);
+            for (int i = 0; i <=cnt; i++)
+            {
+                if (number[i] >= average)
+                {
+                    printf(" %d ",number[i]);
+                }
+            }
+            break;
+        case 4:
+            printf("And the numbers larger than(<=) the average(%f) are:\n", average);
+            for (int i = 0; i <=cnt; i++)
+            {
+                if (number[i] >= average)
+                {
+                    printf(" %d \f",number[i]);
+                }
+            }
+            break;
+        default:
+            printf("Skipped");
+            break;
     }
     return 0;
 }
