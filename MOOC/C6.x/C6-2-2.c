b5d23da972f66575028f750d3d26d9ed0a936d07
hellobbn <clfbbn@gmail.com>
2016-07-20 00:04:47 +0800

    MOOC/C6.x/C6-2-2.c: Update to optimize the source (1/2)

--- MOOC/C6.x/C6-2-2.c
+++ MOOC/C6.x/C6-2-2.c
@@ -5,8 +5,14 @@
 //  Created by BBN on 16/7/19.
 //  Copyright © 2016年 BBN. All rights reserved.
 //
+//  Version: 1.0.1
+//  Update date: 7/19/2016
+//  Status: Staging
+//
+
 
 #include <stdio.h>
+#include <math.h>
 
 //  Function
 int isPrime(int x);
@@ -27,7 +33,10 @@ int main()
 
 int isPrime(int x)
 {
-    for (int i = 2; i < x; i ++) {
+    if (x == 1 || (x % 2 == 0 && x !=2)) {
+        return 0;
+    }
+    for (int i = 2; i < sqrt(x); i += 2) {
         if (x % i == 0) {
             return 0;
         }
