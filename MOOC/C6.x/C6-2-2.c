6ddefc2f155f3229f09699826e85bd99595fadfa
hellobbn <clfbbn@gmail.com>
2016-07-20 10:15:19 +0800

    MOOC/C6.x/C6-2-2.c: Add Modification 	1. Optimism the source though the source will seem longer. 	2. Error Fixes. 	3.Turn it into Beta

--- MOOC/C6.x/C6-2-2.c
+++ MOOC/C6.x/C6-2-2.c
@@ -5,9 +5,9 @@
 //  Created by BBN on 16/7/19.
 //  Copyright © 2016年 BBN. All rights reserved.
 //
-//  Version: 1.0.1
-//  Update date: 7/19/2016
-//  Status: Staging
+//  Version: 1.1.0
+//  Update date: 7/20/2016
+//  Status: Beta
 //
 
 
@@ -15,31 +15,59 @@
 #include <math.h>
 
 //  Function
-int isPrime(int x);
+int isPrime(int input, int All_P[], int num);
 
 int main()
 {
-    int input_number;
-    printf("Enter a number please:");
-    scanf("%d", &input_number);
+    int input_num;
+    int Primes[10000] = {2};
+    int prime_test = 3;
+    int cnt = 1;
     
-    if (isPrime(input_number)) {
-        printf("Yes");
-    }else{
-        printf("No");
+    scanf("%d",&input_num);
+    while (prime_test < input_num)
+    {
+        int judge_Prime = 0;
+        for (int i = 2; i < prime_test; i++)
+        {
+            if (prime_test % i == 0)
+            {
+                judge_Prime = 1;
+                break;
+            }
+        }
+        if (judge_Prime == 0)
+        {
+            Primes[cnt] = prime_test;
+            cnt++;
+        }
+        prime_test++;
     }
+    
+    if (isPrime(input_num, Primes,cnt-1) == 1) {
+        printf("No");
+    }else{
+        printf("Yes");
+    }
+
+//    FOR TEST
+//    for (int x = 0; x < cnt; x++) {
+//        printf("x = %d", x);
+//        printf("%d\n",Primes[x]);
+//    }
+//    printf("%d",Primes[1]);
     return 0;
 }
 
-int isPrime(int x)
+int isPrime(int input, int All_P[],int num)
 {
-    if (x == 1 || (x % 2 == 0 && x !=2)) {
-        return 0;
-    }
-    for (int i = 2; i < sqrt(x); i += 2) {
-        if (x % i == 0) {
-            return 0;
+    int ret = 0;
+    for (int i = 0; i <= num; i ++)
+    {
+//        printf("i = %d, = %d",i, All_P[i]);
+        if (input % All_P[i] == 0) {
+            ret = 1;
         }
     }
-    return 1;
+    return ret;
 }
