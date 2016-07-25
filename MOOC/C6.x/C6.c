42104da317072a18223cd8ae4bc880f8ecfdc5b7
hellobbn <clfbbn@gmail.com>
2016-07-26 00:36:39 +0800

    MOOC/C6.x/C6.c: Update Details: 	 Avoid Output x1 TODO: 	Shorten the source to disable unnecessary staff

--- MOOC/C6.x/C6.c
+++ MOOC/C6.x/C6.c
@@ -7,42 +7,76 @@
 //
 
 #include <stdio.h>
- 
+
 int main()
 {
-	int saver[100];
-	for(int i = 0;i <=100;i++){
-		saver[i] = 0;
-	}
-	int cnt = 0;
-	int a,b = 0;
-	while(cnt < 2)
-	{
-		scanf("%d%d",&a,&b);
-                saver[a] += b;
-		if(a ==0)
-			cnt++;
-	}
-        cnt = 1;
-	for(int i = 100; i >= 0; i --)
-	{
-		if(saver[i]==0)
-				continue;
-		if( i == 0 && cnt != 1 && saver[i] > 0 ){
-			printf("+%dx",saver[i]);}
-		else if(i == 0 && cnt == 1&& saver[i] > 0){
-			printf("%dx",saver[i]);
-		}else if(i == 0 &&  saver[i] < 0){
-			printf("%dx",saver[i]);
-		}else if(i > 0 && cnt == 1){
-		        printf("%dx%d",saver[i],i);
-		}else if(i > 0 && cnt != 1 && saver[i] < 0){
-			printf("%dx%d",saver[i],i);
-		}else if(i > 0 && cnt != 1 && saver[i] > 0){
-			printf("+%dx%d",saver[i],i);
-		}
-
-	}
-	return 0;
-
+    int saver[100];
+    for(int i = 0;i <=100;i++){
+        saver[i] = 0;
+    }
+    int cnt = 0;
+    int a,b = 0;                                                                                                                                                                                                            while(cnt < 2)                                                                                                                                                                                                          {                                                                                                                                                                                                                               scanf("%d%d",&a,&b);                                                                                                                                                                                                    saver[a] += b;                                                                                                                                                                                                          if(a ==0)
+        cnt++;
+    }
+    cnt = 1;
+    for(int i = 100; i >= 0; i --)
+    {
+        if(saver[i]==0&& i!= 0)                                                                                                                                                                                                                 continue;
+        if(saver[i] != 1){
+            if( i == 0 && cnt != 1 && saver[i] >= 0 ){
+                printf("+%d",saver[i]);}
+            else if(i == 0 && cnt == 1&& saver[i] >= 0){
+                printf("%d",saver[i]);
+            }
+            else if(i == 0 &&  saver[i] < 0){
+                printf("%d",saver[i]);
+            }else if(i == 1){
+                if(saver[i] > 0 && cnt != 1){
+                    printf("+%dx",saver[i]);
+                }
+                if(saver[i] < 0 && cnt != 1){
+                    printf("%dx",saver[i]);
+                }
+                if(cnt == 1)
+                    printf("%dx",saver[i]);
+            }
+            else if(i > 0 && cnt == 1){
+                printf("%dx%d",saver[i],i);
+            }else if(i > 0 && cnt != 1 && saver[i] < 0){
+                printf("%dx%d",saver[i],i);
+            }else if(i > 0 && cnt != 1 && saver[i] > 0){
+                printf("+%dx%d",saver[i],i);                                                                                                                                                                                    }
+        }else{
+            if( i == 0 && cnt != 1 && saver[i] >= 0 ){
+                printf("+%d",saver[i]);}
+            else if(i == 0 && cnt == 1&& saver[i] >= 0){
+                printf("%d",saver[i]);
+            }                                                                                                                                                                                   else if(i == 0 &&  saver[i] < 0){
+                printf("%d",saver[i]);
+            }else if(i == 1){
+                if(saver[i] > 0 && cnt != 1){
+                    printf("+x");
+                    
+                }
+                if(saver[i] < 0 && cnt != 1){
+                    
+                    printf("x");
+                    
+                }
+                if(cnt == 1)
+                    
+                    printf("x");
+            }
+            else if(i > 0 && cnt == 1){
+                printf("x%d",i);
+            }else if(i > 0 && cnt != 1 && saver[i] < 0){
+                printf("x%d",i);                                                                                                                                                 }else if(i > 0 && cnt != 1 && saver[i] > 0){
+                    printf("+x%d",i);
+                }
+        }
+        cnt++;
+        
+    }
+    return 0;
+    
 }
