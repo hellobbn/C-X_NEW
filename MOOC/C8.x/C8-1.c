542a6a6f379126722a4df4e9f8fa1adc425906d2
hellobbn <clfbbn@gmail.com>
2016-07-31 07:36:04 +0800

    MOOC/C8.x: Add C8-1.c 	This is a pointer example which is used to exchange numbers.

---
+++ MOOC/C8.x/C8-1.c
@@ -0,0 +1,31 @@
+//
+//  C8-1.c
+//  C-X_NEW
+//
+//  Created by 陈泺帆 on 2016/7/30.
+//  Copyright © 2016年 BBN. All rights reserved.
+//
+//  Pointer Example - 1
+//  Swap numbers
+//
+
+#include <stdio.h>
+
+void swap(int *a,int *b);
+
+int main()
+{
+    int a, b;
+    scanf("%d%d",&a,&b);
+    swap(&a, &b);
+    printf("a' = %d\nb' = %d", a, b);
+    return 0;
+}
+
+void swap(int *a,int *b)
+{
+    int t;
+    t = *a;
+    *a = *b;
+    *b = t;
+}
