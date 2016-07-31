780239aad53a52969c0706d930c70598f10a386c
hellobbn <clfbbn@gmail.com>
2016-07-31 08:56:31 +0800

    MOOC/C8.x: Add C8-3.c: Divide numbers 	Pointer example - 3

---
+++ MOOC/C8.x/C8-3.c
@@ -0,0 +1,37 @@
+//
+//  C8-3.c
+//  C-X_NEW
+//
+//  Created by 陈泺帆 on 2016/7/31.
+//  Copyright © 2016年 BBN. All rights reserved.
+//
+//  Pointer Example - 3
+//  Divide numbers
+//
+
+#include "stdio.h"
+
+double divide(double a, double b, double *re);
+
+int main(int argc, char const *argv[])
+{
+    double a = 2;
+    double b = 5;
+    double result;
+    if (divide(a,b,&result) == 0)
+    {
+        printf("%.0f / %.0f = %.2f\n", a, b, result);
+    }
+    return 0;
+    
+}
+
+double divide(double a, double b, double *re)
+{
+    if (b == 0)
+    {
+        return 1;
+    }
+    *re = a / b;
+    return 0;
+}
