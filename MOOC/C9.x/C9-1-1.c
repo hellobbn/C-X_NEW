8d3076ab8a166752d67ace53eba05aa54c04e9c4
hellobbn <clfbbn@gmail.com>
2016-08-10 23:11:24 +0800

    MOOC/C9.x/C9-1-1.c: Add array s3[]

--- MOOC/C9.x/C9-1-1.c
+++ MOOC/C9.x/C9-1-1.c
@@ -14,10 +14,16 @@ int main()
     int i;
     char *s = "Hello, World!";
     char *s2 = "Hello, World!";
+    char s3[] = "Hello, World!";
     
     printf("&i = %p\n", &i);
     printf("*s = %p\n",s);
     printf("*s2 = %p\n", s2);
+    printf("*s3 = %p\n", s3);
+    printf("s3 = %s\n\n", s3);
+    s3[0] = 'B';
+    printf("Change s3[0] to B\n\n");
+    printf("s3' = %s\n", s3);
     
 //    s[0] = 'h';             // Will FAIL to RUN
     
