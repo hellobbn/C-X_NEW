d2323b5aabc3019ee24eeca1a715cfd48aeb6be9
hellobbn <clfbbn@gmail.com>
2016-08-10 22:34:41 +0800

    MOOC/C9.x: Add C9-1.c: String - 1

---
+++ MOOC/C9.x/C9-1.c
@@ -0,0 +1,22 @@
+//
+//  C9-1.c
+//  C-X_NEW
+//
+//  Created by BBN on 2016/8/10.
+//  Copyright © 2016年 BBN. All rights reserved.
+//
+//  String - 1
+//
+
+#include <stdio.h>
+
+int main()
+{
+//  Example - 1: Define a String:
+    char word[] = {'H','e','a','c','\0'};
+    return 0;
+//  Example 2
+    char *str = "Hello";
+    char woord[] = "Hello";
+    char wrd[10] = "Hello";
+}
