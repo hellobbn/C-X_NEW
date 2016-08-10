d162cd349aeb570e8978af15ecb6aeda13999e9d
hellobbn <clfbbn@gmail.com>
2016-08-10 22:35:20 +0800

    MOOC/C9.x: Add C9-1-1.c: String-2

---
+++ MOOC/C9.x/C9-1-1.c
@@ -0,0 +1,21 @@
+//
+//  C9-1-1.c
+//  C-X_NEW
+//
+//  Created by BBN on 2016/8/10.
+//  Copyright © 2016年 BBN. All rights reserved.
+//
+//  String Example - 1
+
+#include <stdio.h>
+
+int main()
+{
+    char *s = "Hello, World!";
+    char *s2 = "Hello, World!";
+    
+    printf("*s = %p\n",s);
+    printf("*s2 = %p\n", s2);
+    
+    return 0;
+}
