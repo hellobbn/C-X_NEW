a2fc9d29be417547a2ef8589ba4347877f5c2953
hellobbn <clfbbn@gmail.com>
2016-08-11 08:04:08 +0800

    MOOC/C9.x: Add C9-1-2.c

---
+++ MOOC/C9.x/C9-1-2.c
@@ -0,0 +1,20 @@
+//
+//  C9-1-2.c
+//  C-X_NEW
+//
+//  Created by BBN on 2016/8/10.
+//  Copyright © 2016年 BBN. All rights reserved.
+//
+//  String -3
+
+#include <stdio.h>
+
+int main()
+{
+    char *t = "title";
+    char *s;
+    
+    s = t;
+    
+    return 0;
+}
