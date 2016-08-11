ef524ae51cf30d7c05046cb0edd519c750b6ce38
hellobbn <clfbbn@gmail.com>
2016-08-11 10:45:17 +0800

    MOOC/C9.x/C9-2-2.c: Add header file and and entry in main source file

--- MOOC/C9.x/C9-2-2.c
+++ MOOC/C9.x/C9-2-2.c
@@ -8,15 +8,19 @@
 //  String Functions
 //  size_t strlen(const char *s)
 //
+#warning:  Please compile it with C9-2-2-mylen.c and C9-2-2.h!!
 
-#include <stdio.h>
-#include <string.h>
+#include "C9-2-2.h"
 
 int main()
 {
     char line[] = "Hello";
     printf("strlen = %lu\n",strlen(line));
+    printf("mylen = %d\n", mylen(line));
     printf("sizeof = %lu\n",sizeof(line));
     
     return 0;
 }
+
+//  Let's find out how strlen() works!
+//  please see C9-2-2-mylen.c
---
+++ MOOC/C9.x/C9-2-2.h
@@ -0,0 +1,18 @@
+//
+//  C9-2-2.h
+//  C-X_NEW
+//
+//  Created by BBN on 2016/8/11.
+//  Copyright © 2016年 BBN. All rights reserved.
+//
+
+#ifndef C9_2_2_h
+#define C9_2_2_h
+
+
+#endif /* C9_2_2_h */
+
+#include <stdio.h>
+#include <string.h>
+
+int mylen(const char *s);
