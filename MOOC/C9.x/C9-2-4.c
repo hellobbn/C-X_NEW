ec883585dbcee04d94303081330016f1d001986e
hellobbn <clfbbn@gmail.com>
2016-08-11 13:24:25 +0800

    MOOC/C9.x: Add C9-2-4.c C9-2-5.c: Skipped Class

---
+++ MOOC/C9.x/C9-2-4.c
@@ -0,0 +1,20 @@
+//
+//  C9-2-4.c
+//  C-X_NEW
+//
+//  Created by BBN on 2016/8/11.
+//  Copyright © 2016年 BBN. All rights reserved.
+//
+//  String Functions
+//  strcpy()
+//  A safer version strncpy()
+//
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    return 0;
+    //skip
+}
---
+++ MOOC/C9.x/C9-2-5.c
@@ -0,0 +1,19 @@
+//
+//  C9-2-5.c
+//  C-X_NEW
+//
+//  Created by BBN on 2016/8/11.
+//  Copyright © 2016年 BBN. All rights reserved.
+//
+//  String Functions
+//  strcat()
+//  A safer version: strncat()
+//
+
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    //Skip
+    return 0;
+}
