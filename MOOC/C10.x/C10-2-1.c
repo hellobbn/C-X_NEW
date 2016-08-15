0b8fbc8b82cdc7cc5b5c51d05d81bf46cacb817d
hellobbn <clfbbn@gmail.com>
2016-08-15 22:01:35 +0800

    MOOC/C10.x: Add C10-2-1.c -- structure

---
+++ MOOC/C10.x/C10-2-1.c
@@ -0,0 +1,29 @@
+//
+//  C10-2-1.c
+//  C-X_NEW
+//
+//  Created by BBN on 2016/8/15.
+//  Copyright © 2016年 BBN. All rights reserved.
+//
+//  struct E.g.
+//
+
+#include <stdio.h>
+int main()
+{
+    struct date{
+        int month;
+        int day;
+        int year;
+    };
+    
+    struct date today;
+    
+    today.day = 15;
+    today.month = 8;
+    today.year = 2016;
+    
+    printf("The date today is %i-%i-%i.\n",today.year,today.month,today.day);
+    
+    return 0;
+}
