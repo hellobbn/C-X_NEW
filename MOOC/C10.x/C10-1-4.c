c45d82838d73c363d586c7cfd0ac5b3f4633d5dc
hellobbn <clfbbn@gmail.com>
2016-08-15 22:01:07 +0800

    MOOC/C10.x: Add C10-1-4.c

---
+++ MOOC/C10.x/C10-1-4.c
@@ -0,0 +1,18 @@
+//
+//  C10-1-4.c
+//  C-X_NEW
+//
+//  Created by BBN on 2016/8/15.
+//  Copyright © 2016年 BBN. All rights reserved.
+//
+
+#include <stdio.h>
+
+enum colors {RED = 1, YELLOW, GREEN,};
+
+int main()
+{
+    printf("Code for green is %d\n", GREEN);
+    
+    return 0;
+}
