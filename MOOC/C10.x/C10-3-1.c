8239eafb4201ceba38e0954f5713478ebc2fc24a
hellobbn <clfbbn@gmail.com>
2016-08-18 21:22:29 +0800

    Add C10-3-1.c: Skipped of typedef

---
+++ MOOC/C10.x/C10-3-1.c
@@ -0,0 +1,16 @@
+//
+//  C10-3-1.c
+//  C-X_NEW
+//
+//  Created by BBN on 2016/8/18.
+//  Copyright © 2016年 BBN. All rights reserved.
+//
+//	typedef
+//
+
+#include <stdio.h>
+
+int main()
+{
+	return 0;
+}
