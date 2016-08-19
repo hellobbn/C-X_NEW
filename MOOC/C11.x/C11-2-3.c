0f088abb81c65201977f4e5c3415cc50fbe58b2d
hellobbn <clfbbn@gmail.com>
2016-08-19 09:08:10 +0800

    Add C11-2-3.c: Macros - 3 MOOC/C11.x/C11-2-3.c

---
+++ MOOC/C11.x/C11-2-3.c
@@ -0,0 +1,13 @@
+//
+//  C11-2-3.c
+//  C-X_NEW
+//
+//  Created by BBN on 2016/8/19.
+//  Copyright © 2016年 BBN. All rights reserved.
+//
+//	Macros - 3
+//
+
+#include <stdio.h>
+
+#define MIN(a,b) ((a) > (b)? (b):(a))
