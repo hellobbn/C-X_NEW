becac90cdd418dbd77266bbc56ed5e08972c2497
hellobbn <clfbbn@gmail.com>
2016-08-19 09:45:17 +0800

    Add C11-3-*.c: Max example MOOC/C11.x/C11-3-*.c

---
+++ MOOC/C11.x/C11-3-max.c
@@ -0,0 +1,16 @@
+//
+//  C11-3-max.c
+//  C-X_NEW
+//
+//  Created by BBN on 2016/8/19.
+//  Copyright © 2016年 BBN. All rights reserved.
+//
+
+#include "C11-3-max.h"
+
+
+int gAll = 12;
+double max(double x, double y)
+{
+	return x > y?x:y;
+}
---
+++ MOOC/C11.x/C11-3-max.h
@@ -0,0 +1,17 @@
+//
+//  C11-3-max.h
+//  C-X_NEW
+//
+//  Created by BBN on 2016/8/19.
+//  Copyright © 2016年 BBN. All rights reserved.
+//
+
+#ifndef C11_3_max_h
+#define C11_3_max_h
+
+#include <stdio.h>
+
+#endif /* C11_3_max_h */
+
+extern int gAll;
+double max(double x, double y);
