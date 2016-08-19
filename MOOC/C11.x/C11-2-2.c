4c08e5aae5a464229abc4d7f998bc8a84ee346d6
hellobbn <clfbbn@gmail.com>
2016-08-19 08:54:56 +0800

    Add C11-2-2.c: Macros - 2 MOOC/C11.x/C11-2-2.c

---
+++ MOOC/C11.x/C11-2-2.c
@@ -0,0 +1,20 @@
+//
+//  C11-2-2.c
+//  C-X_NEW
+//
+//  Created by BBN on 2016/8/19.
+//  Copyright © 2016年 BBN. All rights reserved.
+//
+//	Macros - 2
+//
+
+#include <stdio.h>
+
+#define cube(x) ((x)*(x)*(x))
+
+int main() {
+	
+	printf("%d\n",cube(5));
+	
+	return 0;
+}
