5727ce5d638686426d3b7f9e123cc3f5b35f3381
hellobbn <clfbbn@gmail.com>
2016-08-19 08:39:29 +0800

    Add C11-2-1.c: Pre-command MOOC/C11.x/C11-2-1.c check: 	gcc --save-temps

---
+++ MOOC/C11.x/C11-2-1.c
@@ -0,0 +1,14 @@
+//
+//  C11-2-1.c
+//  C-X_NEW
+//
+//  Created by BBN on 2016/8/19.
+//  Copyright © 2016年 BBN. All rights reserved.
+//
+//	Pre-command
+//
+
+#include <stdio.h>
+
+#define PI 3.14159
+
