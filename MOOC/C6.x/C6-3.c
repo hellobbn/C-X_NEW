16955277e86a255672849f0094eea42c428c4b02
hellobbn <clfbbn@gmail.com>
2017-08-04 17:04:45 +0800

    C6-3.c: Updated to version1.1 This update makes code change easier

--- MOOC/C6.x/C6-3.c
+++ MOOC/C6.x/C6-3.c
@@ -5,19 +5,20 @@
 //  Created by BBN on 2017/8/4.
 //  Copyright © 2017年 BBN. All rights reserved.
 //
-//  Version 1.0
+//  Version 1.1
 //  Update on 2017-8-4
 
 #include "stdio.h"
 
-int isPrime(int, int [100], int);
+int isPrime(int input, int inPrimeList[], int number);
 
 int main(int argc, char const *argv[])
 {
-    int primeList[100] = {2};
+    const int number = 100;
+    int primeList[number] = {2};
     int cnt = 1;
     int i = 3;
-    while( cnt < 100 )
+    while( cnt < number )
     {
         if (isPrime(i, primeList, cnt) == 1)
         {
@@ -26,7 +27,7 @@ int main(int argc, char const *argv[])
         i++;
     }
     
-    for (cnt = 0; cnt < 100; ++cnt)
+    for (cnt = 0; cnt < number; ++cnt)
     {
         printf("%d\t",primeList[cnt] );
         if ((cnt + 1) % 5 == 0)
@@ -42,7 +43,7 @@ int isPrime(int input, int inPrimeList[], int number)
     int is_true = 1;
     for (int i = 0; i < number; ++i)
     {
-        if (input % inPrimeList[i] == 0)
+        if (input / inPrimeList[i] != 0)
         {
             is_true = 0;
             break;
