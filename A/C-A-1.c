9989c97a2f9afa0ea2b5f82d2261c58c72f0c87b
hellobbn <clfbbn@gmail.com>
2017-09-09 16:53:50 +0800

    Project: Update

---
+++ A/C-A-1.c
@@ -0,0 +1,17 @@
+//
+//  C-A-1.c
+//  C-X_NEW
+//
+//  Created by BBN on 2017/9/9.
+//  Copyright © 2017年 BBN. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include <ctype.h>
+#include <string.h>
+int main() {
+    
+}
--- MOOC/C11.x/C11-4.c
+++ MOOC/C11.x/C11-4.c
@@ -1,54 +1,61 @@
-//
-//  C11-4.c
-//  C-X_NEW
-//
-//  Created by BBN on 2017/8/20.
-//  Copyright © 2017年 BBN. All rights reserved.
-//
-
 #include "stdio.h"
+#include "stdlib.h"
 #include "string.h"
-#include "strings.h"
-
+#include "stdbool.h"
 int main(int argc, char const *argv[])
 {
-    char strBuf[100];
-    char str[1000];
-    do{
-        fgets(strBuf, sizeof(strBuf), stdin);
-        strcat(str, strBuf);
-    } while(strBuf[0] != '\n');
-    
-    //    strtok begin here
-    char delimeters[] = " \".,;:!?)(\n";
-    char* wordsBuf;
-    char* words[1000] = {0};
-    int counter[1000] = {0};
-    wordsBuf = strtok(str, delimeters);
-    words[0] = wordsBuf;
-    counter[0]++;
-    int i = 1;
-    while((wordsBuf = strtok(NULL, delimeters))) {
-//        printf("%s\n", wordsBuf);
-        int isSame = 0;
-        for (int x = 0; x < i && isSame == 0; ++x)        //    This function will find if the word matches the former
+    int cnt = 0;
+    int STR_NUM = 10;                                                //    initialize the number of strings
+    int BUFF_LEN = 100;                                                //    the size of buff
+    int STR_NUM_ADD_PER_STEP = 1;                                    //    when add str_num is needed, it will automatically allocate memory for str
+    char** str = calloc(STR_NUM, sizeof(char*));                    //    a var to store all the strings
+    char buff[BUFF_LEN];                                            //    a var to temporarily store one string -- eaual to char** str
+    char *ptr = NULL;
+    int str_len;
+    while(true)                                                     //    this part will store all the input and automatically use realloc to increase thr memory use
+    {
+        ptr = fgets(buff, BUFF_LEN, stdin);
+        if (!ptr)
         {
-            if (strcasecmp(wordsBuf, words[x]) == 0)
+            printf("error\n");
+        }
+        if (*buff == '\n')
+        {
+            break;
+        }
+        if (cnt == STR_NUM)
+        {
+            STR_NUM += STR_NUM_ADD_PER_STEP;
+            str = realloc(str, STR_NUM);
+        }
+        str_len = strlen(buff);
+        str[cnt] = malloc(str_len);
+        if (!(strcpy(str[cnt], ptr)))
+        {
+            printf("DEBUG: Error copying string from buff to str[%d]\n",cnt );
+            return 1;
+        }
+        cnt += 1;
+    }
+    int job_done = 0;
+    while(!job_done) {
+        for (int i = 0; i < cnt - 1; ++i)
+        {
+            if (strcmp(str[i], str[i + 1]) > 0)
             {
-                counter[x] ++;
-                isSame = 1;
+                char* temp = NULL;
+                temp = str[i];
+                str[i + 1] = str[i];
+                str[i] = temp;
             }
         }
-        if (isSame == 0)
-        {
-            words[i] = wordsBuf;
-            counter[i]++;
-            i++;
-        }
     }
-    for (int x = 0; x < i; ++x)
+    
+    for (int i = 0; i < cnt; ++i)
     {
-        printf("%s\t%d\n",words[x], counter[x] );
+        printf("%s\n", *(str + i));
     }
+    free(str);
+    str = NULL;
     return 0;
 }
