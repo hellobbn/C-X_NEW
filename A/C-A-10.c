ed8e51dc3f4459f092aabc0c3f1e32603e45723c
hellobbn <clfbbn@gmail.com>
2017-10-12 23:15:45 +0800

    Project: Add C-A-10.c When you enter 'a-z' it prints 'abcdefghi....z'

---
+++ A/C-A-10.c
@@ -0,0 +1,47 @@
+//
+//  C-A-10.c
+//  C-X_NEW
+//
+//  Created by clfbbn on 2017/10/12.
+//  Copyright © 2017年 BBN. All rights reserved.
+//
+//  Version: 1.0
+//  Updated: 2017-10-12
+//
+//
+#include <stdio.h>
+
+void expand(char str1[], char str2[], int num_of_str1, int num_of_str2);
+int main() {
+    char s1[10] = {0}, s2[100] = {0};
+    int i = 0;
+    char c = 0;
+    while (((c = getchar()) != EOF) && (i < 9)) {
+        if (c == '\n') {
+            break;
+        }
+        s1[i++] = c;
+    }
+    s1[i] = '\0';
+    expand(s1, s2, i, 100);                    //  Temp debug with number 10 and 100
+    printf("%s\n", s1);
+    printf("%s\n", s2);
+    
+    return 0;
+}
+
+void expand(char str1[], char str2[], int num_of_str1, int num_of_str2) {
+    int start = 0, end = 0;
+    int i = 0;
+    while (str1[i] == ' ') i++  ;                //  If blank -- do nothing!
+    if (str1[i] > 0) {
+        start = str1[i];
+    }
+    if (str1[++i] == '-') {
+        end = str1[++i];
+    }
+    
+    for (int x = start, j = 0; j < end - start + 1; ++x, ++j) {
+        str2[j] = x;
+    }
+}
