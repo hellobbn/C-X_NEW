d041164d0a9bba773df7a7d5875c4255d00cbb4a
hellobbn <clfbbn@gmail.com>
2017-10-13 12:21:22 +0800

    C-A-10.c: Update to version 1.1 1. Added DEBUG flag 2. Added support for input like 'a-z0-9'

--- A/C-A-10.c
+++ A/C-A-10.c
@@ -11,6 +11,8 @@
 //
 #include <stdio.h>
 
+#define DEBUG 1
+
 void expand(char str1[], char str2[], int num_of_str1, int num_of_str2);
 int main() {
     char s1[10] = {0}, s2[100] = {0};
@@ -24,7 +26,9 @@ int main() {
     }
     s1[i] = '\0';
     expand(s1, s2, i, 100);                    //  Temp debug with number 10 and 100
-    printf("%s\n", s1);
+    if (DEBUG) {
+        printf("%s\n", s1);
+    }
     printf("%s\n", s2);
     
     return 0;
@@ -34,14 +38,19 @@ void expand(char str1[], char str2[], int num_of_str1, int num_of_str2) {
     int start = 0, end = 0;
     int i = 0;
     while (str1[i] == ' ') i++  ;                //  If blank -- do nothing!
-    if (str1[i] > 0) {
-        start = str1[i];
+    int x, j = 0;
+    while (str1[i] != '\0') {
+        if (str1[i] > 0) {
+            start = str1[i];
+        }
+        if (str1[++i] == '-') {
+            end = str1[++i];
+        }
+        int temp = j;
+        for (x = start; j - temp < end - start + 1; ++x, ++j) {
+            str2[j] = x;
+        }
+        ++i;
     }
-    if (str1[++i] == '-') {
-        end = str1[++i];
-    }
-    
-    for (int x = start, j = 0; j < end - start + 1; ++x, ++j) {
-        str2[j] = x;
-    }
+
 }
