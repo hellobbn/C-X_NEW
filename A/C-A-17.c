8a1a9d97374d4eff09e57d9fb0445b9c088eb53a
hellobbn <clfbbn@gmail.com>
2017-10-20 21:05:13 +0800

    Project: Update

--- A/C-A-17.c
+++ A/C-A-17.c
@@ -1,12 +1,10 @@
 //
-//  main.c
+//  C-A-17.c
 //  C-X_NEW
 //
-//  Created by clfbbn on 2017/10/16.
+//  Created by clfbbn on 2017/10/17.
 //  Copyright © 2017年 BBN. All rights reserved.
 //
 
 #include <stdio.h>
 
-int main() {
-}
